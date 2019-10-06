245463ac4987d27067b74d6557487acdc91ec513
komo-fr <komo.mdrms@gmail.com>
2019-10-06 23:25:29 +0900

    [AC] D - Disjoint Set of Common Divisors

--- abc/142/d/main.py
+++ abc/142/d/main.py
@@ -7,25 +7,30 @@ c = min(a, b)
 d = max(a, b)
 
 
-def get_divisor(n: int) -> list:
-    d_list = []
-    for i in range(1, int(n ** 1 / 2) + 1):
-        if n % i == 0:
-            a = int(n / i)
-            d_list += [a, i]
-    d_list = list(set(d_list))
-    return d_list
+def factorize(n: int) -> list:
+    # 試し割り法
+    f_list = []
+    while n % 2 == 0:
+        f_list.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            f_list.append(f)
+            n //= f
+        else:
+            # 偶数はすでにチェックしているので+2ずつカウントアップ
+            f += 2
+
+    if n != 1:
+        f_list.append(n)
+    return f_list
 
 
-w_list = get_divisor(c)
-# 正の公約数
-# 以下のリストから任意の数を選ぶ
-koyaku_list = [w for w in w_list if d % w == 0]
+a_list = factorize(a)
+b_list = factorize(b)
 
-
-
-
-    # if fractions.gcd(x, y) == 1:
-    #     count += 1
+common = (set(a_list) & set(b_list)) | {1}
+count = len(common)
 
 print(count)
