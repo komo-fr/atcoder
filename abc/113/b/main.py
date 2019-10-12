dea99b96d98b967580fc1056b883704f003f61af
komo-fr <komo.mdrms@gmail.com>
2019-10-12 20:12:37 +0900

    [AC] B. Palace

--- abc/113/b/main.py
+++ abc/113/b/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+T, A = list(map(int, input().split()))
+h_list = list(map(int, input().split()))
+min_diff = float("inf")
+target = -1
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for i, h in enumerate(h_list):
+    m = T - h * 0.006
+    if abs(m - A) < min_diff:
+        target = i + 1
+        min_diff = abs(m - A)
 
+ans = target
 print(ans)
