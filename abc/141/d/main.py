2e3e12f85dbd9904ec750e09099a0ca7d302421f
komo-fr <komo.mdrms@gmail.com>
2019-10-18 14:46:05 +0900

    [TLE] D - Powerful Discount Tickets

--- abc/141/d/main.py
+++ abc/141/d/main.py
@@ -4,14 +4,8 @@ N, M = list(map(int, input().split()))
 a_list = list(map(int, input().split()))
 
 for _ in range(M):
-    max_diff = -float("inf")
-    max_index = 0
-    for i, a in enumerate(a_list):
-        if a - (a // 2) > max_diff:
-            max_diff = a - (a // 2)
-            max_a = a // 2
-            max_index = i
-    a_list[max_index] = max_a
+    a_list = sorted(a_list, reverse=True)
+    a_list[0] = a_list[0] // 2
 
 ans = sum(a_list)
 print(ans)
