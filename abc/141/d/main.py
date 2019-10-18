fcc140c14b905c8aaff0161ecca51d4eb8b5d92a
komo-fr <komo.mdrms@gmail.com>
2019-10-18 14:30:31 +0900

    [TLE] D - Powerful Discount Tickets

--- abc/141/d/main.py
+++ abc/141/d/main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
+N, M = list(map(int, input().split()))
 a_list = list(map(int, input().split()))
-x_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for _ in range(M):
+    max_diff = -float("inf")
+    max_index = 0
+    for i, a in enumerate(a_list):
+        if a - (a // 2) > max_diff:
+            max_diff = a - (a // 2)
+            max_a = a // 2
+            max_index = i
+    a_list[max_index] = max_a
 
+ans = sum(a_list)
 print(ans)
