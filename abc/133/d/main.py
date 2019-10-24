68ffa7a0def501c0d2239fe432b86b2b1805f3ae
komo-fr <komo.mdrms@gmail.com>
2019-10-24 11:30:53 +0900

    [TLE] D - Rain Flows into Dams

--- abc/133/d/main.py
+++ abc/133/d/main.py
@@ -2,10 +2,19 @@
 
 N = int(input().split()[0])
 a_list = list(map(int, input().split()))
-x_list = []
+start_w = a_list[0] * 2
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+# 一番小さいものから決めていく？
+for sw in range(start_w + 1):
+    w_list = []
+    if a_list[0] < sw // 2:
+        continue
+    w_list.append(sw)
+    for i in range(1, N):
+        w_list.append((a_list[i - 1] - w_list[i - 1] // 2) * 2)
 
+    if a_list[-1] == (w_list[0] + w_list[-1]) // 2:
+        break
+
+ans = " ".join([str(w) for w in w_list])
 print(ans)
