26caf530006a9d99b0e347fff2d51bb9e862aaa1
komo-fr <komo.mdrms@gmail.com>
2019-10-25 09:15:41 +0900

    [WA] D - Insertion

--- abc/064/d/main.py
+++ abc/064/d/main.py
@@ -4,34 +4,11 @@ import collections
 N = int(input().split()[0])
 S = input()
 
-# 外側に括弧をつけた方が辞書順は小さい
-# ['(((()))())', '((()))()()']
-
-# (の数と)の数は一致すべき
-# 最低でも、足りていない方を足りていない分だけ追加することになる
-# 一致している場合でも、OKとは限らない
-# counter = collections.Counter(S)
-# if '(' not in counter.keys() or counter['('] < counter[')']:
-
-# ()
-# ))
-# ((
-# )( がある場合は、そこで一区切りつく
-# )(と()があれば、その間の長さで確定する
-
-w_dict = {}
-j = 0
-before = ""
-ext_n_open = 0
-ext_n_close = 0
+ext_n_open, ext_n_close = 0, 0
 sub_s = ""
 
 for i, ch in enumerate(list(S)):
-    if i == 0:
-        sub_s += ch
-        continue
-
-    if (S[i - 1] == ")" and ch == "(") or i == N - 1:
+    if i == N - 1 or (S[i] == ")" and S[i + 1] == "("):
         # )(だったら
         sub_s += ch
         counter = collections.Counter(sub_s)
