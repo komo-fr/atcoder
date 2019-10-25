d1c74a8d38f8829862ac180a442f0b0a981c71e3
komo-fr <komo.mdrms@gmail.com>
2019-10-25 10:49:45 +0900

    [AC] D - Insertion

--- abc/064/d/main.py
+++ abc/064/d/main.py
@@ -8,27 +8,16 @@ ext_n_open, ext_n_close = 0, 0
 sub_s = ""
 
 for i, ch in enumerate(list(S)):
-    if i == N - 1 or (S[i] == ")" and S[i + 1] == "("):
-        # )(だったら
-        sub_s += ch
-        counter = collections.Counter(sub_s)
-        if counter["("] > counter[")"]:
-            ext_n_open += counter["("] - counter[")"]
-        elif counter["("] < counter[")"]:
-            n_close_w = counter[")"] - counter["("]
-            n_close = max(0, n_close_w - ext_n_open)
-            ext_n_open = max(0, ext_n_open - n_close_w)
-            ext_n_close += n_close
-        sub_s = ""
+    if ch == "(":
+        ext_n_open += 1
     else:
-        # )), ((, ()
-        sub_s += ch
+        if ext_n_open == 0:
+            # 余分な)である
+            ext_n_close += 1
+        else:
+            # 以前出た(と相殺
+            ext_n_open -= 1
 
-if ext_n_open < ext_n_close:
-    ans = "(" * (ext_n_close - ext_n_open) + S
-elif ext_n_open > ext_n_close:
-    ans = S + ")" * (ext_n_open - ext_n_close)
-else:
-    ans = "(" * ext_n_open + S + ")" * ext_n_close
+ans = "(" * ext_n_close + S + ")" * ext_n_open
 
 print(ans)
