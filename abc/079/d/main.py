040f1cc559b967ab6eeacdf3cbc7d2ec2902c54f
komo-fr <komo.mdrms@gmail.com>
2019-10-29 15:01:59 +0900

    [AC] D - Wall

--- abc/079/d/main.py
+++ abc/079/d/main.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
+import numpy as np
+from scipy.sparse.csgraph import shortest_path
 
-N = int(input().split()[0])
 H, W = list(map(int, input().split()))
 
 c_table = []
 
-for _ in range(9):
-    c_table += list(map(int, input().split()))
+for _ in range(10):
+    c_table.append(list(map(int, input().split())))
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+a_table = []
 
+for _ in range(H):
+    a_table.append(list(map(int, input().split())))
+
+c_table = np.array(c_table)
+a_table = np.array(a_table)
+
+d_matrix = shortest_path(c_table)
+cost = 0
+
+for h in range(H):
+    a_list = a_table[h]
+    for a in a_list:
+        if a == -1:
+            continue
+        cost += d_matrix[a][1]
+
+ans = int(cost)
 print(ans)
