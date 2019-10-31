5c8fe274a6f58542efed791f338c0d6a4334ed53
komo-fr <komo.mdrms@gmail.com>
2019-10-31 19:36:22 +0900

    [AC] A - 動く歩道

--- arc/arc054/a/main.py
+++ arc/arc054/a/main.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+L, X, Y, S, D = list(map(int, input().split()))
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+clockwise_s = X + Y
+anti_clockwise_s = Y - X
+
+if S == D:
+    ans = 0
+else:
+    if S > D:
+        anti_clockwise_l = S - D
+        clockwise_l = L - anti_clockwise_l
+    else:
+        clockwise_l = D - S
+        anti_clockwise_l = abs(clockwise_l - L)
+
+    if anti_clockwise_s > 0:
+        ans = min(clockwise_l / clockwise_s, anti_clockwise_l / anti_clockwise_s)
+    else:
+        ans = clockwise_l / clockwise_s
 
 print(ans)
