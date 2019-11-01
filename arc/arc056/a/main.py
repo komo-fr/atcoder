a5be21fd33b85d9218e40323683a3bc38a9d2342
komo-fr <komo.mdrms@gmail.com>
2019-11-01 16:09:31 +0900

    [AC] A - みんなでワイワイみかん

--- arc/arc056/a/main.py
+++ arc/arc056/a/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+A, B, K, L = list(map(int, input().split()))
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+if K % L == 0:
+    n = K // L
+    cost = B * n
+else:
+    b_set_n = K // L
+    a_n = K - L * b_set_n
+    cost_1 = B * b_set_n + A * a_n
+    cost_2 = B * (b_set_n + 1)
+    cost = min(cost_1, cost_2)
 
+ans = cost
 print(ans)
