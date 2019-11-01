df751c18353a1f39e4d2e6a8be9f5492148264ee
komo-fr <komo.mdrms@gmail.com>
2019-11-01 16:18:27 +0900

    setup

--- arc/arc055/a/main.py
+++ arc/arc055/a/main.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+ans = 10 ** N + 7
 print(ans)
