09a2a2a98919c42280445b2199394c2f2f62b0eb
komo-fr <komo.mdrms@gmail.com>
2019-11-01 15:28:59 +0900

    [WA] A - 2兆円

--- arc/arc057/a/main.py
+++ arc/arc057/a/main.py
@@ -7,8 +7,8 @@ goal = 2 * 10 ** 12
 
 if K == 0:
     d = goal - A
-elif K == 0:
-    d = math.ceil(math.log(goal / A, K + 1)) + 1
+elif A == 0:
+    d = math.ceil(math.log(goal, K + 1)) + 1
 else:
     d = math.ceil(math.log(goal / A, K + 1))
 
