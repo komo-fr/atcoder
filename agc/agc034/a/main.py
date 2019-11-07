21cea85d1133917198803d1d7748bdf495389381
komo-fr <komo.mdrms@gmail.com>
2019-11-07 20:36:36 +0900

    [WA] A - Kenken Race

--- agc/agc034/a/main.py
+++ agc/agc034/a/main.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, A, B, C, D = list(map(int, input().split()))
+S = input()
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+is_ok = True
+
+for i in range(A, max(C, D) - 1):
+    if S[i] == "#" and S[i + 1] == "#":
+        is_ok = False
+        break
+
+if is_ok:
+    if C > D:
+        is_ok = False
+        for i in range(B, D - 1):
+            if S[i] == "." and S[i + 1] == "." and S[i + 2] == ".":
+                is_ok = True
+                break
+
+ans = "Yes" if is_ok else "No"
 
 print(ans)
