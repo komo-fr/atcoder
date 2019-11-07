3b068fda3a2acdd6f7c4818a0511190184ec18ca
komo-fr <komo.mdrms@gmail.com>
2019-11-07 21:25:21 +0900

    [AC] A - Biscuits

--- agc/agc017/a/main.py
+++ agc/agc017/a/main.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python3
+import math
 
-N = int(input().split()[0])
+N, P = list(map(int, input().split()))
 a_list = list(map(int, input().split()))
-x_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+odd_n = 0
+even_n = 0
+
+for a in a_list:
+    if a % 2 == 0:
+        even_n += 1
+    else:
+        odd_n += 1
+
+
+def C(n, r):
+    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+
+
+if P == 0:
+    odd_p = 1
+    if odd_n > 0:
+        # 偶数個選ぶ組み合わせ
+        for i in range(2, odd_n + 1, 2):
+            odd_p += C(odd_n, i)
+    even_p = 2 ** even_n
+    p = odd_p * even_p
+else:
+    odd_p = 0
+    if odd_n > 0:
+        # 奇数個選ぶ組み合わせ数
+        for i in range(1, odd_n + 1, 2):
+            odd_p += C(odd_n, i)
+
+    even_p = 2 ** even_n
+    p = odd_p * even_p
+
+ans = p
 
 print(ans)
