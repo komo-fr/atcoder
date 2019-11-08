6bc890800f0a38b99504edfcaf8ec3b562766b56
komo-fr <komo.mdrms@gmail.com>
2019-11-08 19:53:42 +0900

    [WA] B - ABC

--- agc/agc034/b/main.py
+++ agc/agc034/b/main.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+s = input()
+S = list(s)
+N = len(S)
+count = 0
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for i in range(N - 2):
+    sub_s = S[i] + S[i + 1] + S[i + 2]
+    if sub_s == "ABC":
+        S[i] = "B"
+        S[i + 1] = "C"
+        S[i + 2] = "A"
+        count += 1
 
+S = list(reversed(S))
+for i in range(N - 2):
+    sub_s = S[i + 2] + S[i + 1] + S[i]
+    if sub_s == "ABC":
+        S[i + 2] = "B"
+        S[i + 1] = "C"
+        S[i] = "A"
+        count += 1
+ans = count
 print(ans)
