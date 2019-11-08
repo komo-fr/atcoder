41cc6862a482bce9808d933f34bb90fea85c3441
komo-fr <komo.mdrms@gmail.com>
2019-11-08 16:36:54 +0900

    [TLE] B - Box and Ball

--- agc/agc002/b/main.py
+++ agc/agc002/b/main.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, M = list(map(int, input().split()))
+xy_table = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for _ in range(M):
+    x, y = list(map(int, input().split()))
+    xy_table.append((x, y))
+
+red_box_list = [1]
+# 添字0はダミー
+# 添字 = 箱の番号として扱う
+n_box_list = [0] + [1] * N
+
+for xy in xy_table:
+    x, y = xy
+    n_box_list[x] -= 1
+    n_box_list[y] += 1
+    if x in red_box_list:
+        if y not in red_box_list:
+            red_box_list.append(y)
+
+        if n_box_list[x] == 0:
+            red_box_list.remove(x)
+
+ans = len(red_box_list)
 
 print(ans)
