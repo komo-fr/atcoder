b32fd512ff79fd390261aa6f0f33e95b38dad83e
komo-fr <komo.mdrms@gmail.com>
2019-11-08 19:13:26 +0900

    [AC] A - Zero-Sum Ranges

--- agc/agc023/a/main.py
+++ agc/agc023/a/main.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+import collections
+import math
+
+
+def C(n, r):
+    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+
 
 N = int(input().split()[0])
 a_list = list(map(int, input().split()))
-x_list = []
+cumsum_list = [0]
+for i, a in enumerate(a_list):
+    idx = i + 1
+    c = cumsum_list[idx - 1] + a
+    cumsum_list.append(c)
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+counter = collections.Counter(cumsum_list)
+total = 0
+for k, v in counter.items():
+    if v > 1:
+        total += C(v, 2)
 
+ans = total
 print(ans)
