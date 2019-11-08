42ea54bf2d26f1e879376cda85cf93e0b078e853
komo-fr <komo.mdrms@gmail.com>
2019-11-08 16:50:01 +0900

    [AC] B - Box and Ball

--- agc/agc002/b/main.py
+++ agc/agc002/b/main.py
@@ -7,7 +7,8 @@ for _ in range(M):
     x, y = list(map(int, input().split()))
     xy_table.append((x, y))
 
-red_box_list = [1]
+red_box_dict = {i: False for i in range(1, N + 1)}
+red_box_dict[1] = True
 # 添字0はダミー
 # 添字 = 箱の番号として扱う
 n_box_list = [0] + [1] * N
@@ -16,13 +17,12 @@ for xy in xy_table:
     x, y = xy
     n_box_list[x] -= 1
     n_box_list[y] += 1
-    if x in red_box_list:
-        if y not in red_box_list:
-            red_box_list.append(y)
+    if red_box_dict[x]:
+        red_box_dict[y] = True
 
         if n_box_list[x] == 0:
-            red_box_list.remove(x)
+            red_box_dict[x] = False
 
-ans = len(red_box_list)
+ans = len([v for v in red_box_dict.values() if v])
 
 print(ans)
