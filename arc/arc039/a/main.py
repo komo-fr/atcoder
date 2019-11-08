c16a6e76abeb679b338f70f07a44697dbc1b716d
komo-fr <komo.mdrms@gmail.com>
2019-11-08 16:01:21 +0900

    [AC] A - A - B problem

--- arc/arc039/a/main.py
+++ arc/arc039/a/main.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 A, B = list(map(int, input().split()))
+
 after_A = str(A)
 after_B = str(B)
 if A != 999:
     for i in range(3):
-        if str(A)[i] != 9:
+        if str(A)[i] != "9":
             after_A = str(A)[:i] + "9"
             if i != 2:
                 after_A += str(A)[i + 1 :]
             break
 if B != 100:
-    if str(B)[0] != 1:
+    if str(B)[0] != "1":
         after_B = "1" + str(B)[1:]
     else:
-        if str(B)[1] != 0:
+        if str(B)[1] != "0":
             after_B = str(B)[0] + "0" + str(B)[2]
         elif after_B[2] != 0:
             after_B = str(B)[:2] + "0"
