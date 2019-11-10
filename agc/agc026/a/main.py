988bac7f0b6a8c3279189b24e063baaea20cc124
komo-fr <komo.mdrms@gmail.com>
2019-11-10 10:41:16 +0900

    [AC] A - Candy Distribution Again

--- agc/agc026/a/main.py
+++ agc/agc026/a/main.py
@@ -2,10 +2,10 @@
 
 N = int(input().split()[0])
 a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+count = 0
+for i in range(1, N):
+    if a_list[i-1] == a_list[i]:
+        a_list[i] = -1
+        count += 1
+ans = count
 print(ans)
