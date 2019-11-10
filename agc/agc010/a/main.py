8fca1aea2ae87bd34031bb098c912644268f7170
komo-fr <komo.mdrms@gmail.com>
2019-11-10 11:32:25 +0900

    [AC] A - Addition

--- agc/agc010/a/main.py
+++ agc/agc010/a/main.py
@@ -2,10 +2,16 @@
 
 N = int(input().split()[0])
 a_list = list(map(int, input().split()))
-x_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+odd_n = 0
+even_n = 0
+
+for a in a_list:
+    if a % 2 == 1:
+        odd_n += 1
+    else:
+        even_n += 1
+
+ans = "YES" if odd_n % 2 == 0 else "NO"
 
 print(ans)
