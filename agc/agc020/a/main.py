8d1d1a21e12a9f443633582bea61f72bfaff2436
komo-fr <komo.mdrms@gmail.com>
2019-11-10 11:10:38 +0900

    [AC] A - Move and Win

--- agc/agc020/a/main.py
+++ agc/agc020/a/main.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, A, B = list(map(int, input().split()))
+a_idx = A
+b_idx = B
+turn = "A"
+while True:
+    # 判定
+    if turn == "A":
+        if a_idx == 1 and b_idx == a_idx + 1:
+            winner = "Borys"
+            break
+    else:
+        if b_idx == N and a_idx == b_idx - 1:
+            winner = "Alice"
+            break
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+    if turn == "A":
+        if b_idx == a_idx + 1:
+            a_idx -= 1
+        else:
+            a_idx += 1
+        turn = "B"
+    else:
+        if a_idx == b_idx - 1:
+            b_idx += 1
+        else:
+            b_idx -= 1
+        turn = "A"
 
+ans = winner
 print(ans)
