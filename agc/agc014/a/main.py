b7738b482a579d9ace2256a1072d2e00799a33a4
komo-fr <komo.mdrms@gmail.com>
2019-11-10 11:40:48 +0900

    [AC] A - Cookie Exchanges

--- agc/agc014/a/main.py
+++ agc/agc014/a/main.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+A, B, C = list(map(int, input().split()))
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
 
+def is_continue(x_list):
+    for x in x_list:
+        if x % 2 == 1:
+            return False
+    return True
+
+
+count = 0
+
+while is_continue([A, B, C]):
+    if A == B and B == C:
+        count = -1
+        break
+    a, b, c = A, B, C
+    a = B // 2 + C // 2
+    b = A // 2 + C // 2
+    c = A // 2 + B // 2
+    A, B, C = a, b, c
+    count += 1
+
+ans = count
 print(ans)
