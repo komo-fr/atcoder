223d318e886800ae9fa33f790b073efb776235b0
komo-fr <komo.mdrms@gmail.com>
2019-11-18 22:42:52 +0900

    [AC] A - Add Sub Mul

--- abc/098/a/main.py
+++ abc/098/a/main.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+A, B = list(map(int, input().split()))
+ans = max([A + B, A - B, A * B])
 print(ans)
