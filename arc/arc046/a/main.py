f158d0f83208c5487fc1459bd84b13c62fdbdd0c
komo-fr <komo.mdrms@gmail.com>
2019-11-19 16:50:33 +0900

    [AC] A - ゾロ目数

--- arc/arc046/a/main.py
+++ arc/arc046/a/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+keta = N // 9
+hasu = N % 9
+hasu = 9 if hasu == 0 else hasu
+keta = keta - 1 if hasu == 9 else keta
+
+ans = str(hasu) * (keta + 1)
 
 print(ans)
