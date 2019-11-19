7917cccb4fb5edbd5fca4361ac217ed5fa84c62b
komo-fr <komo.mdrms@gmail.com>
2019-11-19 17:00:05 +0900

    [AC] A - たこ焼き買えるかな？

--- arc/arc008/a/main.py
+++ arc/arc008/a/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+a_1 = (N // 10) * 100 + (N % 10) * 15
+a_2 = ((N // 10) + 1) * 100
+ans = min(a_1, a_2)
 
 print(ans)
