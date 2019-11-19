c9e328d06e77bf94282d0f3554cfdefd292e6adf
komo-fr <komo.mdrms@gmail.com>
2019-11-19 16:41:58 +0900

    [AC] A -  何期生？

--- arc/arc052/a/main.py
+++ arc/arc052/a/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+S = input()
+n_list = []
+for char in list(S):
+    if char in [str(i) for i in range(10)]:
+        n_list.append(char)
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+ans = "".join(n_list)
 
 print(ans)
