9f144e5b7b1a1dff2975ff8802e24336ae481a96
komo-fr <komo.mdrms@gmail.com>
2019-11-19 17:33:54 +0900

    [AC] A - 宝くじ

--- arc/arc006/a/main.py
+++ arc/arc006/a/main.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+e_list = list(map(int, input().split()))
+b = int(input().split()[0])
+l_list = list(map(int, input().split()))
+c = 0
+for e in e_list:
+    if e in l_list:
+        c += 1
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+if c == 6:
+    ans = 1
+elif c == 5:
+    ans = 2 if b in l_list else 3
+elif c < 3:
+    ans = 0
+else:
+    ans = 4 if c == 4 else 5
 
 print(ans)
