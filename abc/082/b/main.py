8cdf7def311670518e656fc84ef4180f75b5d25a
komo-fr <komo.mdrms@gmail.com>
2019-12-19 10:02:05 +0900

    [AC] B - Two Anagrams

--- abc/082/b/main.py
+++ abc/082/b/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+s = input()
+t = input()
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+s = "".join(list(sorted(s)))
+t = "".join(list(sorted(t, reverse=True)))
+ans = "Yes" if s < t else "No"
 print(ans)
