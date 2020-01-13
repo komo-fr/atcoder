d641bfac7a66e58909cd745f3c676c429c5c6d72
komo-fr <komo.mdrms@gmail.com>
2020-01-13 20:00:37 +0900

    [AC] B - Colorful Creatures

--- agc/agc011/b/main.py
+++ agc/agc011/b/main.py
@@ -8,30 +8,28 @@ win_c = 0
 total_size = sum(a_list)
 max_size_list = []
 
-if N != 2:
-    lose_flag = False
-    now_size = 0
-    first_winner = -1
-    for i, a in enumerate(a_list):
-        if i == N - 1:
-            break
-        # 確実に倒せるやつはどいつか
-        now_size += a
-        if now_size * 2 >= a_list[i + 1]:
-            # 勝てる
-            if lose_flag:
-                first_winner = i
-                break
-        else:
-            lose_flag = True
-    c = N if first_winner == -1 else N - first_winner
-else:
-    a, b = a_list[0], a_list[1]
-    if a <= b * 2 and b <= a * 2:
-        c = 2
-    elif a <= b * 2 or b <= a * 2:
-        c = 1
+lose_flag = False
+now_size = 0
+winner = -1
+# 自分より左側のモンスターは確実に吸収できるので
+# 自分と自分より左側のモンスターのサイズの総和で右隣のモンスターを吸収できるか考える
+#
+
+for i, a in enumerate(a_list):
+    if i == N - 1:
+        # 最後は確実に勝つ
+        if winner == -1:
+            winner = i
+        break
+
+    now_size += a
+    if now_size * 2 >= a_list[i + 1]:
+        # 勝てる
+        if winner == -1:
+            winner = i
     else:
-        c = 0
+        winner = -1
+c = N - winner
+
 ans = c
 print(ans)
