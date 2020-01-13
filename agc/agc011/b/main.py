b74a31367da5ad6bed6c8b5f806ea25a07146212
komo-fr <komo.mdrms@gmail.com>
2020-01-13 19:42:31 +0900

    [WIP] B - Colorful Creatures

--- agc/agc011/b/main.py
+++ agc/agc011/b/main.py
@@ -4,16 +4,27 @@ N = int(input().split()[0])
 a_list = list(map(int, input().split()))
 a_list = list(sorted(a_list))
 
-c = 0
+win_c = 0
 total_size = sum(a_list)
+max_size_list = []
 
 if N != 2:
+    lose_flag = False
+    now_size = 0
+    first_winner = -1
     for i, a in enumerate(a_list):
-        # 最終的に勝たないといけないモンスター
-        last_size = a_list[-2] if i == N - 1 else a_list[-1]
-        size = total_size - last_size
-        if last_size <= size * 2:
-            c += 1
+        if i == N - 1:
+            break
+        # 確実に倒せるやつはどいつか
+        now_size += a
+        if now_size * 2 >= a_list[i + 1]:
+            # 勝てる
+            if lose_flag:
+                first_winner = i
+                break
+        else:
+            lose_flag = True
+    c = N if first_winner == -1 else N - first_winner
 else:
     a, b = a_list[0], a_list[1]
     if a <= b * 2 and b <= a * 2:
