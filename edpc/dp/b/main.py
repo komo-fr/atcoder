2c6e8c95ca9cc9ccafb1fe8682aea963dc818289
komo-fr <komo.mdrms@gmail.com>
2020-01-19 11:40:10 +0900

    [AC] B - Frog 2 (1904ms)

--- edpc/dp/b/main.py
+++ edpc/dp/b/main.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
+import numpy as np
 
 N, K = list(map(int, input().split()))
 h_list = list(map(int, input().split()))
-dp_list = [float("inf")] * N
+h_list = np.array(h_list)
+dp_list = np.zeros(N)
 dp_list[0] = 0
 
 for i in range(1, N):
     # j番目の足場からi番目の足場に飛ぶ
-    min_cost = float("inf")
-    for j in range(max(i - K, 0), i):
 
-        # j番目の足場までの最適コスト
-        cum_cost = dp_list[j]
-
-        # j番目の足場から飛んだ場合のコスト
-        cost = dp_list[j] + abs(h_list[j] - h_list[i])
-        min_cost = min(cost, min_cost)
-
+    min_j = max(i - K, 0)
+    cost_list = dp_list[min_j:i] + np.abs(h_list[i] - h_list[min_j:i])
     # 最適コストで更新
-    dp_list[i] = min_cost
+    dp_list[i] = np.min(cost_list)
 
-ans = dp_list[-1]
+ans = int(dp_list[-1])
 print(ans)
