4eaf497827a778617db3a0520cabce6f65c595df
komo-fr <komo.mdrms@gmail.com>
2020-01-19 11:11:35 +0900

    [TLE] B - Frog 2

--- edpc/dp/b/main.py
+++ edpc/dp/b/main.py
@@ -7,17 +7,18 @@ dp_list[0] = 0
 
 for i in range(1, N):
     # j番目の足場からi番目の足場に飛ぶ
-
-    for j in range(max(i - K, 0), i + 1):
+    min_cost = float("inf")
+    for j in range(max(i - K, 0), i):
 
         # j番目の足場までの最適コスト
         cum_cost = dp_list[j]
 
         # j番目の足場から飛んだ場合のコスト
         cost = dp_list[j] + abs(h_list[j] - h_list[i])
+        min_cost = min(cost, min_cost)
 
-        # 最適コストで更新
-        dp_list[i] = min(dp_list[i], cost)
+    # 最適コストで更新
+    dp_list[i] = min_cost
 
 ans = dp_list[-1]
 print(ans)
