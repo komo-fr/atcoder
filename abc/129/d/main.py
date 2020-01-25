d7b04500f2d3b71214e9f33a14f0edab171caa66
komo-fr <komo.mdrms@gmail.com>
2020-01-25 19:18:01 +0900

    [WA] D - Lamp

--- abc/129/d/main.py
+++ abc/129/d/main.py
@@ -7,35 +7,32 @@ total_table = []
 c_table = []
 for _ in range(H):
     s = list(input())
-    s_table.append(s)
+    s_num = [0 if ch == "#" else 1 for ch in s]
+    s_table.append(s_num)
 
-c_table = np.zeros((H, W, 4))
-total_table = np.zeros((H, W))
+s_table = np.pad(s_table, 1, "constant")
 
-for i in range(H):
-    for j in range(W):
-        if j != 0:
-            # 左側の数を数える
-            if s_table[i][j - 1] == "." and s_table[i][j] == ".":
-                c_table[i][j][0] = c_table[i][j - 1][0] + 1
-                total_table[i][j] += c_table[i][j][0]
-        if j != 0:
-            # 右側の数を数える
-            if s_table[i][W - j] == "." and s_table[i][W - j - 1] == ".":
-                c_table[i][W - j - 1][1] = c_table[i][W - j][1] + 1
-                total_table[i][W - j - 1] += c_table[i][W - j - 1][1]
-        if i != 0:
-            # 上側の数を数える
-            if s_table[i - 1][j] == "." and s_table[i][j] == ".":
-                c_table[i][j][2] = c_table[i - 1][j][2] + 1
-                total_table[i][j] += c_table[i][j][2]
-        if i != 0:
-            # 下側の数を数える
-            if s_table[H - i][j] == "." and s_table[H - i - 1][j] == ".":
-                c_table[H - i - 1][j][3] = c_table[H - i][j][3] + 1
-                total_table[H - i - 1][j] += c_table[H - i - 1][j][3]
+l_table = np.zeros((H + 2, W + 2))
+r_table = np.zeros((H + 2, W + 2))
+u_table = np.zeros((H + 2, W + 2))
+b_table = np.zeros((H + 2, W + 2))
 
-ans = np.max(total_table) + 1
-ans = int(ans)
+for i in range(1, W):
+    l_table[:, i] = (l_table[:, i - 1] + 1) * s_table[:, i]
+    r_table[:, -i - 1] = (r_table[:, -i] + 1) * s_table[:, -i - 1]
+
+
+for i in range(1, H):
+    u_table[i] = (u_table[i - 1] + 1) * s_table[i]
+    b_table[-i - 1] = (b_table[-i] + 1) * s_table[-i - 1]
+
+total_table = l_table + r_table + u_table + b_table
+ans = np.max(total_table)
+
+if H <= 1:
+    ans += 2
+if W <= 1:
+    ans += 2
+ans = int(ans) - 3
 
 print(ans)
