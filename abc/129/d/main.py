48d196ed25c265f776aa9b106f6dd2e9416f13dd
komo-fr <komo.mdrms@gmail.com>
2020-01-25 20:07:51 +0900

    [WIP] D - Lamp

--- abc/129/d/main.py
+++ abc/129/d/main.py
@@ -27,12 +27,20 @@ for i in range(1, H):
     b_table[-i - 1] = (b_table[-i] + 1) * s_table[-i - 1]
 
 total_table = l_table + r_table + u_table + b_table
-ans = np.max(total_table)
 
-if H <= 1:
-    ans += 2
-if W <= 1:
-    ans += 2
-ans = int(ans) - 3
+minus_table = np.zeros((H + 2, W + 2))
+minus_table[2:H, 2:W] = 3
+minus_table[1, 2:W] = 2
+minus_table[-2, 2:W] = 2
+minus_table[2:H, 1] = 2
+minus_table[2:H, -2] = 2
+minus_table[1, 1] = 1
+minus_table[1, -2] = 1
+minus_table[-2, 1] = 1
+minus_table[-2, -2] = 1
+
+total_table = total_table - minus_table
+ans = np.max(total_table)
+ans = int(ans)
 
 print(ans)
