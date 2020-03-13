91064c68cb1fa9915c2ea5b156dd394516043dd8
komo-fr <komo.mdrms@gmail.com>
2020-03-13 22:11:53 +0900

    B - 105

--- abc/106/b/main.py
+++ abc/106/b/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+total = 0
+for i in range(1, N + 1):  # max = 200
+    a = i
+    if a % 2 == 0:
+        continue
+    c = 0
+    for j in range(1, N + 1):  # max = 200
+        if a % j == 0:
+            c += 1
+    if c == 8:
+        total += 1
+ans = total
 print(ans)
