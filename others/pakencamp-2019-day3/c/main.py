360325af93db0d0189110cce948211d7ad4ea27c
komo-fr <komo.mdrms@gmail.com>
2020-03-21 20:49:50 +0900

    [AC] パ研杯2019 C - カラオケ

--- others/pakencamp-2019-day3/c/main.py
+++ others/pakencamp-2019-day3/c/main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+import itertools
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
+N, M = list(map(int, input().split()))
+a_table = []
+c = 0
 for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+    a_list = list(map(int, input().split()))
+    a_table.append(a_list)
 
-print(ans)
+p_gen = itertools.combinations(range(M), 2)
+max_score = -float("inf")
+for p in p_gen:
+    a, b = p
+    temp_score = 0
+    for a_list in a_table:
+        temp_score += max(a_list[a], a_list[b])
+    max_score = max(temp_score, max_score)
+print(max_score)
