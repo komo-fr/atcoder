5306314f1e0858463bf921f1dac8956d7dfe87e5
komo-fr <komo.mdrms@gmail.com>
2020-04-02 11:41:40 +0900

    [AC] A - 掲示板

--- arc/arc042/a/main.py
+++ arc/arc042/a/main.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 
-N, M = list(map(int, input().split()))
+N, M = list(map(int, input().split()))  # 10**5, 10**5
 a_list = []
 
 for _ in range(M):
     a = int(input().split()[0])
     a_list.append(a)
 
-now_list = [x for x in range(1, N + 1)]
+done_dict = {k: False for k in range(1, N + 1)}  # 10 ** 5
+now_list = []
 
-for a in a_list:
-    now_list.remove(a)
-    now_list.insert(0, a)
+for a in a_list[::-1]:  # 10 ** 5
+    if done_dict[a]:
+        continue
+
+    now_list.append(a)
+    done_dict[a] = True
+
+for k in range(1, N + 1):  # 10 ** 5
+    if done_dict[k]:
+        continue
+
+    now_list.append(k)
 
 ans = "\n".join([str(a) for a in now_list])
 
