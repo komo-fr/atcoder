30bd6801919fae4730f84b032f4544d2278511a0
komo-fr <komo.mdrms@gmail.com>
2020-04-02 11:28:20 +0900

    [TLE] A - 掲示板

--- arc/arc042/a/main.py
+++ arc/arc042/a/main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, M = list(map(int, input().split()))
+a_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for _ in range(M):
+    a = int(input().split()[0])
+    a_list.append(a)
+
+now_list = [x for x in range(1, N + 1)]
+
+for a in a_list:
+    now_list.remove(a)
+    now_list.insert(0, a)
+
+ans = "\n".join([str(a) for a in now_list])
 
 print(ans)
