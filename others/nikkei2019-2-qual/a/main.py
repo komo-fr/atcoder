6addfa28ba52f68c562ed2a65c7f06a80cff90f2
komo-fr <komo.mdrms@gmail.com>
2020-04-03 08:19:24 +0900

    [AC] A - Sum of Two Integers

--- others/nikkei2019-2-qual/a/main.py
+++ others/nikkei2019-2-qual/a/main.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+ans = (N - 1) // 2
 print(ans)
