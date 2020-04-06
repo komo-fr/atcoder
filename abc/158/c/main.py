42809f6380c17dc6f2180590b0ed961f2ba20479
komo-fr <komo.mdrms@gmail.com>
2020-04-07 08:51:30 +0900

    [AC]C - Tax Increase

--- abc/158/c/main.py
+++ abc/158/c/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+A, B = list(map(int, input().split()))
+x = -1
+for i in range(1, 10000):
+    if int(i * 0.08) == A and int(i * 0.1) == B:
+        x = i
+        break
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+ans = x
 print(ans)
