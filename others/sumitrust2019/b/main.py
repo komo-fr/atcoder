723de9d788a121ca3d0f5c4478181a6fc8e84672
komo-fr <komo.mdrms@gmail.com>
2020-04-17 22:56:22 +0900

    [AC] B - Tax Rate

--- others/sumitrust2019/b/main.py
+++ others/sumitrust2019/b/main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+import math
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+X = N / 1.08
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+if math.floor(math.ceil(X) * 1.08) == N:
+    ans = math.ceil(X)
+elif math.floor(math.floor(X) * 1.08) == N:
+    ans = math.floor(X)
+else:
+    ans = ":("
 
 print(ans)
