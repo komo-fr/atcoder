64c2e134b0efd1dfb9ddcf450286c419d4793771
komo-fr <komo.mdrms@gmail.com>
2020-04-20 23:13:13 +0900

    [AC] A - ヘイホー君と最終試験

--- others/code-festival-2015-morning-middle/a/main.py
+++ others/code-festival-2015-morning-middle/a/main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+import math
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, K, M, R = list(map(int, input().split()))
+s_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for _ in range(N - 1):
+    s = int(input().split()[0])
+    s_list.append(s)
+
+s_list = sorted(s_list, reverse=True)
+w_1 = sum(s_list[:K])
+w_2 = sum(s_list[: K - 1])
+
+if w_1 / K >= R:
+    ans = 0
+elif (w_2 + M) / K < R:
+    ans = -1
+else:
+    ans = int(math.ceil(R * K - w_2))
 
 print(ans)
