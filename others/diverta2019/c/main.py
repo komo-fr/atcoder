9da5e9f811f8a569359fdf65e4cfec75f37d0411
komo-fr <komo.mdrms@gmail.com>
2020-04-25 23:00:30 +0900

    C - AB Substrings

--- others/diverta2019/c/main.py
+++ others/diverta2019/c/main.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
+s_list = []
+a_list = []
+b_list = []
+ab_list = []
+ab_count = 0
 for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+    s = input()
+    s_list.append(s)
+    ab_count += s.count("AB")
+    if s[0] == "B" and s[-1] == "A":
+        ab_list.append(s)
+    elif s[0] == "B":
+        b_list.append(s)
+    elif s[-1] == "A":
+        a_list.append(s)
 
+if a_list:
+    if ab_list:
+        a_list.pop()
+        ab_count += len(ab_list)
+        if b_list:
+            ab_count += 1
+            b_list.pop()
+        ab_count += max(min(len(a_list), len(b_list)), 0)
+    else:
+        ab_count += min(len(a_list), len(b_list))
+elif b_list:
+    ab_count += len(ab_list)
+else:
+    ab_count += max(len(ab_list) - 1, 0)
+
+ans = ab_count
 print(ans)
