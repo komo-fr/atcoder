1d01266d872446c06b68719cf91f737dd765d574
komo-fr <komo.mdrms@gmail.com>
2020-04-27 21:29:50 +0900

    [AC] B - ニコニコ文字列

--- others/dwango2015-prelims/b/main.py
+++ others/dwango2015-prelims/b/main.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+S = input()
+N = len(S)
+is_seq = False
+count25 = 0
+sub25_len = 0
+for i in range(1, N):
+    ss = S[i - 1] + S[i]
+    if ss == "25":
+        is_seq = True
+        sub25_len += 1
+    elif is_seq and ss == "52":
+        continue
+    else:
+        if is_seq:
+            count25 += sum(list(range(sub25_len + 1)))
+        is_seq = False
+        sub25_len = 0
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+if is_seq:
+    count25 += sum(list(range(sub25_len + 1)))
+ans = count25
 
 print(ans)
