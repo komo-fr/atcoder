90667c9c6e739978bc06f3eeb0a6cc2f20628cde
komo-fr <komo.mdrms@gmail.com>
2020-05-17 18:06:59 +0900

    [AC] A - STring

--- agc/agc005/a/main.py
+++ agc/agc005/a/main.py
@@ -7,7 +7,7 @@ next_i = 0
 # 連続する箇所で部分文字列を作りたい
 s_count, t_count = 0, 0
 if X[0] == "T":
-    # Tから始まる場合、最初の連続するTは確定で残る
+    # Tから始まる場合、最初の連続するTは確定で残るので切り捨てる
     for i, x in enumerate(X):
         if x == "T":
             after_x += x
@@ -16,6 +16,7 @@ if X[0] == "T":
     next_i = i
 
 if len(after_x) != N:
+    # 「全ての文字列がT」ではない
     # 残りの文字列について操作する（最初のTは処理したので、必ずSから始まる）
     for i, char in enumerate(X[next_i:]):
         if i == 0:
@@ -30,7 +31,7 @@ if len(after_x) != N:
                 if t_count >= s_count:
                     # Sを全て使い切り、余ったTは確定で残る
                     after_x += "T" * (t_count - s_count)
-                    t_count -= s_count
+                    t_count = 0
                     s_count = 0
                 else:
                     # Tを全部使い切り、余ったSは引き継がれる
@@ -40,7 +41,6 @@ if len(after_x) != N:
         else:
             # Tの場合
             t_count += 1
-    is_last_process = True
     last_char = char
 
     if last_char == "T":
@@ -58,6 +58,6 @@ if len(after_x) != N:
     else:
         # Sで終わる場合
         after_x += "S" * s_count
-
+# print(after_x)
 ans = len(after_x)
 print(ans)
