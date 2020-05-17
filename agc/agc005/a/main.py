e724ddc9b5420d034a8e2f100e1135e9b28ea23e
komo-fr <komo.mdrms@gmail.com>
2020-05-17 17:43:56 +0900

    [WA] A - STring

--- agc/agc005/a/main.py
+++ agc/agc005/a/main.py
@@ -1,11 +1,59 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+X = input()
+N = len(X)
+after_x = ""
+next_i = 0
+# 連続する箇所で部分文字列を作りたい
+if X[0] == "T":
+    # Tから始まる場合、最初の連続するTは確定で残る
+    for i, x in enumerate(X):
+        if x == "T":
+            after_x += x
+        else:
+            break
+    next_i = i
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+s_count = 0
+t_count = 0
+if next_i < N - 1:
+    # 残りの文字列について操作する（最初のTは処理したので、必ずSから始まる）
+    is_t = False
+    for i, char in enumerate(X[next_i:]):
+        if i == 0:
+            # 最初は確定でSから始まる
+            s_count += 1
+            continue
 
+        if char == "S":
+            # Sの場合
+            if X[next_i + i - 1] == "T":
+                # T->Sの切り替わりのとき
+                if t_count >= s_count:
+                    # Sを全て使い切り、余ったTは確定で残る
+                    after_x += "T" * (t_count - s_count)
+                    t_count -= s_count
+                    s_count = 0
+                else:
+                    # Tを全部使い切り、余ったSは引き継がれる
+                    s_count -= t_count
+                    t_count = 0
+            s_count += 1
+        else:
+            # Tの場合
+            t_count += 1
+
+# 最後に
+if t_count >= s_count:
+    # Sを全て使い切り、余ったTは確定で残る
+    after_x += "T" * (t_count - s_count)
+    t_count -= s_count
+    s_count = 0
+else:
+    # Tを全部使い切り、余ったSは引き継がれる
+    s_count -= t_count
+    t_count = 0
+    after_x += "S" * s_count
+
+ans = len(after_x) + s_count
 print(ans)
