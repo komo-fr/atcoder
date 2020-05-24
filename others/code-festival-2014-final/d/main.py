9a56ec60742dda5057747df5b59960c45875f94e
komo-fr <komo.mdrms@gmail.com>
2020-05-24 21:47:11 +0900

    [AC] D - パスカルの三角形

--- others/code-festival-2014-final/d/main.py
+++ others/code-festival-2014-final/d/main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
-print(ans)
+A = int(input().split()[0])
+y = str(A + 1)
+x = str(2)
+print(y + " " + x)
