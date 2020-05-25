ec82df5c02fae56c09d44d8ea39cefe3ae9d4efe
komo-fr <komo.mdrms@gmail.com>
2020-05-25 22:49:00 +0900

    [AC] B - Robot Arms

--- others/keyence2020/b/main.py
+++ others/keyence2020/b/main.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+xlt_list = []
 
 for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+    x, l = list(map(int, input().split()))
+    xlt_list.append((x, l, x + l))
 
+xlt_list = sorted(xlt_list, key=lambda x: x[2])
+count = 0
+before_t = -float("inf")
+for i, xlt in enumerate(xlt_list):
+    x, l, t = xlt
+    if x - l >= before_t:
+        count += 1
+        before_t = t
+
+ans = count
 print(ans)
