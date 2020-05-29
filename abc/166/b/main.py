96a5ca38e92565785656b44586e8ee200cb665ff
komo-fr <komo.mdrms@gmail.com>
2020-05-29 23:46:16 +0900

    B - Trick or Treat

--- abc/166/b/main.py
+++ abc/166/b/main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+import itertools
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, K = list(map(int, input().split()))
+s_list = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for _ in range(K):
+    _ = input()
+    a_list = list(map(int, input().split()))
+    s_list.append(a_list)
+
+s_list = set(itertools.chain.from_iterable(s_list))
+ans = N - len(s_list)
 
 print(ans)
