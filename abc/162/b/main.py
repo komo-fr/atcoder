0e285e984c49f844607071a3c2183cc035a6add9
komo-fr <komo.mdrms@gmail.com>
2020-05-30 21:01:46 +0900

    [AC] B - FizzBuzz Sum

--- abc/162/b/main.py
+++ abc/162/b/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+total = 0
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for i in range(1, N + 1):
+    if i % 5 != 0 and i % 3 != 0:
+        total += i
 
+ans = total
 print(ans)
