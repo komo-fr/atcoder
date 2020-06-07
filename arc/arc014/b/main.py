46668a8ba8092b5e71ecdfcfc3053b545b0dc42a
komo-fr <komo.mdrms@gmail.com>
2020-06-07 18:06:01 +0900

    [AC] B - あの日したしりとりの結果を僕達はまだ知らない。

--- arc/arc014/b/main.py
+++ arc/arc014/b/main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+w_list = []
+r = "DRAW"
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for i in range(N):
+    w = input()
+    if i == 0:
+        w_list.append(w)
+        continue
 
+    if w[0] != w_list[i - 1][-1] or w in w_list:
+        if i % 2 == 0:
+            r = "LOSE"
+        else:
+            r = "WIN"
+        break
+    w_list.append(w)
+ans = r
 print(ans)
