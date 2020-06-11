4eeac31124f72422c7eda7769e960133ce5c2ce8
komo-fr <komo.mdrms@gmail.com>
2020-06-11 23:51:46 +0900

    [AC] B - RGB Boxes

--- others/diverta2019/b/main.py
+++ others/diverta2019/b/main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+R, G, B, N = list(map(int, input().split()))
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+max_r = N // R
+max_g = N // G
+max_b = N // B
+
+count = 0
+for r in range(max_r + 1):
+    for g in range(max_g + 1):
+        now_n = r * R + g * G
+        if now_n <= N and (N - now_n) % B == 0:
+            b = (N - now_n) // B
+            count += 1
+
+ans = count
 
 print(ans)
