506168d044a78b4d63d4517af789365910e99cf2
komo-fr <komo.mdrms@gmail.com>
2020-06-20 22:07:10 +0900

    [WA] C - Guess The Number

--- abc/157/c/main.py
+++ abc/157/c/main.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, M = list(map(int, input().split()))
+sc_list = []
+for _ in range(M):
+    s, c = list(map(int, input().split()))
+    sc_list.append((s, c))
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+sc_set = set(sc_list)
+start = 10 ** (N - 1)
+end = 10 ** N
 
+is_ok = True
+
+if len(sc_set) > N:
+    is_ok = False
+
+answer = -1
+if is_ok:
+    for i in range(start, end):
+        is_sub_ok = True
+        str_number = str(i)
+        for sc in sc_set:
+            s, c = sc
+            if str_number[s - 1] != str(c):
+                is_sub_ok = False
+                continue
+        if is_sub_ok:
+            answer = i
+            break
+ans = answer
 print(ans)
