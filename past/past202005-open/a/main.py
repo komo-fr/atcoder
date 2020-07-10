9515b76a55e136152d2d08701041048408fe0f44
komo-fr <komo.mdrms@gmail.com>
2020-07-10 22:05:12 +0900

    [AC] A - ケース・センシティブ

--- past/past202005-open/a/main.py
+++ past/past202005-open/a/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+s = input()
+t = input()
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
-print(ans)
+if s == t:
+    print("same")
+elif s.lower() == t.lower():
+    print("case-insensitive")
+else:
+    print("different")
