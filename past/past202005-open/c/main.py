3dbf1cb0c853b15cdc7b4868ef498a8fc74b3aee
komo-fr <komo.mdrms@gmail.com>
2020-07-18 12:05:57 +0900

    [AC] C - 等比数列

--- past/past202005-open/c/main.py
+++ past/past202005-open/c/main.py
@@ -2,8 +2,9 @@
 import math
 
 A, R, N = list(map(int, input().split()))
-# a = A * (R ** (N - 1))
-# ans = a if a <= 10 ** 9 else "large"
-ans = A * (R ** (N - 1)) if N <= math.log((10 ** 9 / A), R) + 1 else "large"
+if R == 1:
+    ans = A if A <= 10 ** 9 else "large"
+else:
+    ans = A * (R ** (N - 1)) if N <= math.log((10 ** 9 / A), R) + 1 else "large"
 
 print(ans)
