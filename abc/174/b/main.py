a9157df76cf084d3e6d97551ce537bff620ba2a1
komo-fr <komo.mdrms@gmail.com>
2020-09-02 08:12:32 +0900

    [AC] B - Distance

--- abc/174/b/main.py
+++ abc/174/b/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import math
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+N, D = list(map(int, input().split()))
+xy_list = []
 
+count = 0
 for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
-print(ans)
+    x, y = list(map(int, input().split()))
+    if math.sqrt(x ** 2 + y ** 2) <= D:
+        count += 1
+print(count)
