20baf69b9de1669f118c3a9bc168b085bebeaeb1
komo-fr <komo.mdrms@gmail.com>
2020-09-04 23:04:27 +0900

    [AC] B - Minor Change

--- abc/172/b/main.py
+++ abc/172/b/main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
+S = input()
+T = input()
+count = 0
+for s, t in zip(S, T):
+    if s != t:
+        count += 1
+ans = count
 print(ans)
