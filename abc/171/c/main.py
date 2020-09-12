f47014526c76a14c0751e041e84a8736d86ae51f
komo-fr <komo.mdrms@gmail.com>
2020-09-12 17:57:27 +0900

    [AC] C - One Quadrillion and One Dalmatians

--- abc/171/c/main.py
+++ abc/171/c/main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-from cmath import e
 import string
 
 N = int(input().split()[0])
-a_list = string.ascii_lowercase
+a_list = list(string.ascii_lowercase)
 
 amari_list = []
 n = N
-while n != 0:
-    quo = n // 26
+while n > 0:
+    n -= 1
     amari = n % 26
-    amari_list.append(amari - 1)
-    n = quo
+    amari_list.append(a_list[amari])
+    n = n // 26
 
-ans = "".join([a_list[amari] for amari in reversed(amari_list)])
+ans = "".join(reversed(amari_list))
 
 print(ans)
