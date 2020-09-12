32953398e979ab022222025db25675409344270f
komo-fr <komo.mdrms@gmail.com>
2020-09-12 17:08:47 +0900

    [WA] C - One Quadrillion and One Dalmatians

--- abc/171/c/main.py
+++ abc/171/c/main.py
@@ -4,34 +4,15 @@ import string
 
 N = int(input().split()[0])
 a_list = string.ascii_lowercase
-w = 26
-i = 1
 
-while N > w:
-    i += 1
-    w += 26 ** i
-    print(i)
-    print(w)
-
-keta = i
-w = N
 amari_list = []
-print(f"keta={keta}")
-while keta > 0:
-    print(w)
+n = N
+while n != 0:
+    quo = n // 26
+    amari = n % 26
+    amari_list.append(amari - 1)
+    n = quo
 
-    amari = w % (26 ** (keta - 1))
-    amari_list.append(amari)
-    w = w // (26 ** (keta - 1))
-    keta -= 1
+ans = "".join([a_list[amari] for amari in reversed(amari_list)])
 
-text = ""
-print(amari_list)
-for i, amari in enumerate(amari_list):
-    if i == len(amari_list) - 1:
-        text += a_list[amari - 1]
-    else:
-        text += a_list[amari]
-
-ans = text
 print(ans)
