0ac8db490f073c42eacf92369418b8f0aa2d1d56
komo-fr <komo.mdrms@gmail.com>
2020-10-12 23:03:39 +0900

    [AC] D - Friends

--- abc/177/d/main.py
+++ abc/177/d/main.py
@@ -6,31 +6,36 @@ N, M = list(map(int, input().split()))
 
 class UnionFind:
     def __init__(self, n):
-        self.parent = list(range(n))
+        self.parents = list(range(n))
 
     def find(self, i):
-        if self.parent[i] == i:
+        if self.parents[i] == i:
             # 自分自身だったらそのまま返す
             return i
         else:
             # 自分の祖先を、自分の祖先の祖先で更新する
             # こうすることで、途中を飛ばすことができる
-            self.parent[i] = self.find(self.parent[i])
-            return self.parent[i]
+            self.parents[i] = self.find(self.parents[i])
+            return self.parents[i]
 
     def union(self, i, j):
         i = self.find(i)
         j = self.find(j)
-        self.parent[j] = i
+        if i != j:
+            # これがないとrandom_11.txtとrandom_12.txtでREになる
+            i, j = min(i, j), max(i, j)
+
+            # 別の家族だったら養子に迎える
+            self.parents[j] = i
 
 
-union = UnionFind(N)
+uf = UnionFind(N)
 
 for _ in range(M):
     s, t = list(map(int, input().split()))
-    union.union(s - 1, t - 1)
+    uf.union(s - 1, t - 1)
 
-group = [union.find(i) for i in range(N)]
+group = [uf.find(i) for i in range(N)]
 counter = collections.Counter(group)
 ans = counter.most_common()[0][1]
 
