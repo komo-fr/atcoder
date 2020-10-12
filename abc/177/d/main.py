0bdb7592ac5b21b98fa127f38f1d8a59a390a7c9
komo-fr <komo.mdrms@gmail.com>
2020-10-12 21:32:11 +0900

    [TLE] D - Friends

--- abc/177/d/main.py
+++ abc/177/d/main.py
@@ -2,17 +2,20 @@
 import networkx as nx
 
 N, M = list(map(int, input().split()))
-edge_list = []
-G = nx.Graph()
+edge_set = set()
+# edge_set = []
 for _ in range(M):
     s, t = list(map(int, input().split()))
-    # s, t = min(s, t), max(s, t)
-    edge_list.append((s, t))
+    s, t = min(s, t), max(s, t)
+    edge_set = edge_set | {(s, t)}
+    # edge_set.append((s, t))
 
 group_list = []
 group_dict = {i + 1: None for i in range(N)}
 
-for s, t in edge_list:  # 10 ** 5
+for edge in edge_set:  # 10 ** 5
+    s, t = edge
+    # print(f"edge={edge}")
     if (group_dict[s] is None) and (group_dict[t] is None):
         group_list.append({s, t})
         new_group = len(group_list) - 1
@@ -22,18 +25,29 @@ for s, t in edge_list:  # 10 ** 5
         group_index = group_dict[s]
         group_list[group_index] = group_list[group_index] | {s, t}
         if group_dict[t] is not None:
-            other_group = group_list[group_dict[t]]
+            other_group_index = group_dict[t]
+            if other_group_index == group_index:
+                continue
+
+            other_group = group_list[other_group_index]
             group_list[group_index] = group_list[group_index] | other_group
-            group_list[group_dict[t]] = set()
+            for i in group_list[other_group_index]:
+                group_dict[i] = group_index
+            group_list[other_group_index] = set()
         group_dict[t] = group_index
     else:
         group_index = group_dict[t]
         group_list[group_index] = group_list[group_index] | {s, t}
         group_dict[s] = group_index
 
+    # print(group_list)
+    # print(group_dict)
+    # print("----------")
+
 max_group_size = 1
 for group in group_list:
     max_group_size = max(len(group), max_group_size)
 
 ans = max_group_size
+
 print(ans)
