144e9e7c74bcabb2162efba2f93dc776b258fed2
komo-fr <komo.mdrms@gmail.com>
2020-10-13 21:40:10 +0900

    [AC] D - Alter Altar

--- abc/174/d/main.py
+++ abc/174/d/main.py
@@ -1,30 +1,13 @@
 #!/usr/bin/env python3
 
+import collections
+
 N = int(input().split()[0])
 c_input = input()
-l_white_count = 0
-r_red_count = 0
-swap_count = 0
 
-
-for i in range(N // 2 + 1):
-    l = i
-    r = (N - 1) - i
-
-    if c_input[l] == "W":
-        l_white_count += 1
-    if l != r and c_input[r] == "R":
-        r_red_count += 1
-    if r_red_count > 0 and l_white_count > 0:
-        swap_count += 1
-        l_white_count -= 1
-        r_red_count -= 1
-
-if N % 2 != 0:
-    if c_input[N // 2] == "W" and r_red_count > 0:
-        swap_count += 1
-    elif c_input[N // 2] == "R" and l_white_count > 0:
-        swap_count += 1
-
-ans = swap_count
+counter = collections.Counter(c_input)
+red_n = counter["R"]
+counter = collections.Counter(c_input[:red_n])
+left_white_n = counter["W"]
+ans = left_white_n
 print(ans)
