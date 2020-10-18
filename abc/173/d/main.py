bfe0e314256cc35461640cf0991be52de0b49c18
komo-fr <komo.mdrms@gmail.com>
2020-10-18 18:25:14 +0900

    [AC] D - Chat in a Circle

--- abc/173/d/main.py
+++ abc/173/d/main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+from collections import deque
 
 N = int(input().split()[0])
 a_list = list(map(int, input().split()))
-min_a = min(a_list)
+a_list = sorted(a_list, reverse=True)
 
-ans = min_a * (N - 1)
+c_list = a_list[: (N + 1) // 2]
+
+if len(c_list) == 1:
+    ans = c_list[0]
+elif N % 2 == 0:
+    ans = c_list[0] + sum(c_list[1:]) * 2
+else:
+    ans = c_list[0] + sum(c_list[1:-1]) * 2 + c_list[-1]
 
 print(ans)
