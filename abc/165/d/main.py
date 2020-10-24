1d7a921a84dba8aa4c565e836027d54ff9db6486
komo-fr <komo.mdrms@gmail.com>
2020-10-24 11:22:39 +0900

    [AC] D - Floor Function

--- abc/165/d/main.py
+++ abc/165/d/main.py
@@ -3,20 +3,11 @@ import math
 
 A, B, N = list(map(int, input().split()))
 
-# Test
-max_score = -float("inf")
-for i in range(1, N + 1):
-    if i > B:
-        break
-    f_1 = math.floor(A * i / B)
-    f_2 = A * math.floor(i / B)
+x = min(N, B - 1)
+f_1 = math.floor(A * x / B)
+f_2 = A * math.floor(x / B)
 
-    score = f_1 - f_2
-    max_score = max(score, max_score)
-    # print(f"x = {i}: {f_1} - {f_2} = {p}")
-    # print(f"A * x / B = {A * i} / {B} = {A* i/B}")
-    # print(f"x / B     = {i / B}")
-    # print("========================")
+score = f_1 - f_2
 
-ans = max_score
+ans = score
 print(ans)
