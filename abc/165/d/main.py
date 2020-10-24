0f5bc4cbf4f0ebaa912a71ef57671bd03f2f1f4f
komo-fr <komo.mdrms@gmail.com>
2020-10-24 11:11:52 +0900

    [TLE] D - Floor Function

--- abc/165/d/main.py
+++ abc/165/d/main.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+import math
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+A, B, N = list(map(int, input().split()))
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+# Test
+max_score = -float("inf")
+for i in range(1, N + 1):
+    if i > B:
+        break
+    f_1 = math.floor(A * i / B)
+    f_2 = A * math.floor(i / B)
 
-ans =
+    score = f_1 - f_2
+    max_score = max(score, max_score)
+    # print(f"x = {i}: {f_1} - {f_2} = {p}")
+    # print(f"A * x / B = {A * i} / {B} = {A* i/B}")
+    # print(f"x / B     = {i / B}")
+    # print("========================")
+
+ans = max_score
 print(ans)
