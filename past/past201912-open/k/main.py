20bf5fc0dec852fee619d78154858d7bc715d809
komo-fr <komo.mdrms@gmail.com>
2020-10-31 11:43:12 +0900

    [RE] K - 巨大企業

--- past/past201912-open/k/main.py
+++ past/past201912-open/k/main.py
@@ -2,23 +2,59 @@
 from collections import defaultdict
 
 N = int(input().split()[0])
-boss_dict = defaultdict(lambda: defaultdict(lambda: False))
 p_dict = {}
+root_id = None
+child_dict = defaultdict(lambda: defaultdict(lambda: None))
+
 for i in range(N):  # 150000
     p = int(input())
-    p_dict[i + 1] = p
+    child = i + 1
+    if p == -1:
+        root_id = child
+        continue
+
+    if child_dict[p]:
+        child_dict[p].append(child)
+    else:
+        child_dict[p] = [child]
+
+route = []
+appear_dict = {}
+count = 0
+
+
+def walk(node_id: int):
+    global count
+    count += 1
+    global route
+    route.append(node_id)
+    global appear_dict
+    appear_dict[node_id] = dict(first=count, end=None)
+    for child_node_id in child_dict[node_id]:
+        walk(child_node_id)
+    count += 1
+    route.append(node_id)
+    appear_dict[node_id]["last"] = count
+
+
+# print(f"root_id={root_id}")
+# print(child_dict[root_id])
+walk(root_id)
+
+# for k, v in appear_dict.items():
+#     print(f"{k}: {v}")
+
 
 Q = int(input().split()[0])
 
 for _ in range(Q):  # 100000
     a, b = list(map(int, input().split()))
-    me = a
-    is_boss = False
-    while me != -1:
-        me = p_dict[me]
-        if me == b:
-            is_boss = True
-            break
-
-    text = "Yes" if is_boss else "No"
-    print(text)
+    if (
+        appear_dict[b]["first"]
+        <= appear_dict[a]["first"]
+        <= appear_dict[a]["last"]
+        <= appear_dict[b]["last"]
+    ):
+        print("Yes")
+    else:
+        print("No")
