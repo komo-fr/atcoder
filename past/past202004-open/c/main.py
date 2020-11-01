aac3518d14c2a4acdf7ff419f9738e9bd2f15711
komo-fr <komo.mdrms@gmail.com>
2020-11-01 22:01:10 +0900

    [AC] C - 山崩し

--- past/past202004-open/c/main.py
+++ past/past202004-open/c/main.py
@@ -1,12 +1,40 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+s_table = []
 
 for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+    s = input()
+    s_table.append(s)
 
-ans =
-print(ans)
+
+for i in range(N - 1, -1, -1):
+    if i == N - 1:
+        continue
+    start = (N - i) - 1
+    end = N + i
+    preset = "." * start
+    t = preset
+
+    for j in range(start, end):
+        if (
+            s_table[i + 1][j - 1] == "X"
+            or s_table[i + 1][j] == "X"
+            or s_table[i + 1][j + 1] == "X"
+        ):
+            # print("True")
+            out = "X"
+            t += "X"
+        else:
+            out = s_table[i][j]
+            t += s_table[i][j]
+        # print("================")
+        # print(
+        #     f"({i+1}, {j-1}), ({i+1}, {j}), ({i+1}, {j+1})------> {s_table[i+1][j-1]}{s_table[i+1][j]}{s_table[i+1][j+1]} -> {out}"
+        # )
+
+    t += preset
+    s_table[i] = t
+
+for s in s_table:
+    print(s)
