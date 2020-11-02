677e054adbf100f4f1dda67192697e47a24d2550
komo-fr <komo.mdrms@gmail.com>
2020-11-02 23:09:09 +0900

    [WA] F - 回文行列

--- past/past202005-open/f/main.py
+++ past/past202005-open/f/main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+s_table = []
 
 for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+    s = list(input())
+    s_table.append(s)
 
-ans =
-print(ans)
+# print(s_table)
+s_table = [list(x) for x in zip(*s_table)]
+# print(s_table)
+found_flag = False
+for s_list in s_table:
+    r_s_list = list(reversed(s_list))
+    # print(s_list)
+    # print(r_s_list)
+    if s_list == r_s_list:
+        print("".join(s_list))
+        found_flag = True
+        break
+
+if not found_flag:
+    print(-1)
