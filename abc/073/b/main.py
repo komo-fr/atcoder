4b1e01b7012db55a0332ceb1115704c9542d497b
komo-fr <komo.mdrms@gmail.com>
2020-11-02 22:35:47 +0900

    [AC] B - Theater

--- abc/073/b/main.py
+++ abc/073/b/main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
-
+count = 0
 for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+    l, r = list(map(int, input().split()))
+    count += r - l + 1
 
-ans =
+ans = count
 print(ans)
