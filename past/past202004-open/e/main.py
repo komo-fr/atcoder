830957adb43604fc4d67300f1fd79b40fadbb9ec
komo-fr <komo.mdrms@gmail.com>
2020-11-05 21:02:20 +0900

    [AC] E - 順列

--- past/past202004-open/e/main.py
+++ past/past202004-open/e/main.py
@@ -2,11 +2,30 @@
 
 N = int(input().split()[0])
 a_list = list(map(int, input().split()))
-x_list = []
+x2ax_map = {}
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for i, a in enumerate(a_list):
+    x = i + 1
+    x2ax_map[x] = a
 
-ans =
+# print(x2ax_map)
+
+ans_list = []
+for x in range(1, N + 1):
+    # print(f"i={x}")
+    # j = 1
+    if x == x2ax_map[x]:
+        j = 1
+        ans_list.append(str(j))
+        continue
+    start_x = x
+    j = 1
+    while start_x != x2ax_map[x]:
+        j += 1
+        # print(f"[{j}] x, ax = ({x}, {x2ax_map[x]})")
+        ax = x2ax_map[x]
+        x = ax
+    # j += 1
+    ans_list.append(str(j))
+ans = " ".join(ans_list)
 print(ans)
