3ff2e51407ade12dfc7f05906c66076be3b29b10
komo-fr <komo.mdrms@gmail.com>
2020-11-05 22:22:12 +0900

    [AC] F - タスクの消化

--- past/past202004-open/f/main.py
+++ past/past202004-open/f/main.py
@@ -3,29 +3,21 @@ from collections import defaultdict, deque
 import bisect
 
 N = int(input().split()[0])
-ab_list = []
 
-day2p_dict = defaultdict(lambda: deque([]))
+day2p_dict = defaultdict(lambda: [])
 
 for _ in range(N):  # 2 * 10**5
     a, b = list(map(int, input().split()))
-    ab_list.append((a, b))
     if day2p_dict[a]:
-        bisect.insort_right(day2p_dict[a], b)
+        day2p_dict[a].append(b)
     else:
-        day2p_dict[a] = deque([b])
-# print(day2p_dict)
+        day2p_dict[a] = [b]
+
 total = 0
-for i in range(N):  # 2 * 10 ** 5
+p_deque = deque([])
+for i in range(N):
     d = i + 1
-    max_point = -float("inf")
-    max_point_day = -1
-    # print(f"day={i}")
-    for j in range(d + 1):
-        if day2p_dict[j]:
-            if day2p_dict[j][0] > max_point:
-                max_point = day2p_dict[j][0]
-                max_point_day = j
-    p = day2p_dict[max_point_day].pop()
-    total += p
+    for p in day2p_dict[d]:
+        bisect.insort_right(p_deque, p)
+    total += p_deque.pop()
     print(total)
