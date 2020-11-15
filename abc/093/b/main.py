caf5415dbc1ad0536b8b76ed7b87cd3d31eba8ad
komo-fr <komo.mdrms@gmail.com>
2020-11-15 21:38:19 +0900

    [AC] B - Small and Large Integers

--- abc/093/b/main.py
+++ abc/093/b/main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+A, B, K = list(map(int, input().split()))
+done_list = []
+for i in range(A, A + K):
+    if i > B:
+        continue
+    print(i)
+    done_list.append(i)
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
-ans =
-print(ans)
+for i in range(B - K + 1, B + 1):
+    if i < A:
+        continue
+    if i not in done_list:
+        print(i)
