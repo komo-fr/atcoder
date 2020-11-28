13b9a59bce0ddbb47f75e522c73a9a35a9e88834
komo-fr <komo.mdrms@gmail.com>
2020-11-28 20:28:25 +0900

    [AC] B - 仲良しうさぎ

--- others/code-festival-2016-quala/b/main.py
+++ others/code-festival-2016-quala/b/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 
 N = int(input().split()[0])
 a_list = list(map(int, input().split()))
-x_list = []
-
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
-ans =
+friend_dict = defaultdict(lambda: False)
+count = 0
+for i, a in enumerate(a_list):
+    b = i + 1
+    key = tuple(sorted([a, b]))
+    if friend_dict[key]:
+        count += 1
+    else:
+        friend_dict[key] = True
+ans = count
 print(ans)
