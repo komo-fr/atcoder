6004637ea49472d7dcaec4355aa01aec3239afad
komo-fr <komo.mdrms@gmail.com>
2020-12-02 22:49:57 +0900

    [AC] B - Grid Compression

--- abc/107/b/main.py
+++ abc/107/b/main.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
-N = int(input().split()[0])
-a_list = list(map(int, input().split()))
-x_list = []
+H, W = list(map(int, input().split()))
+a_table = []
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
+for _ in range(H):
+    a_list = input()
+    if set(a_list) == {"."}:
+        continue
+    a_table.append(a_list)
 
-ans =
-print(ans)
+a_table = list(zip(*a_table))
+b_table = []
+for a_list in a_table:
+    if set(a_list) == {"."}:
+        continue
+    b_table.append(a_list)
+
+b_table = list(zip(*b_table))
+for b in b_table:
+    print("".join(b))
