8719351c7b6942991b617e267b11afa7adcbf49b
komo-fr <komo.mdrms@gmail.com>
2020-12-04 20:22:14 +0900

    [AC] B - Kleene Inversion

--- others/jsc2019-qual/b/main.py
+++ others/jsc2019-qual/b/main.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 
-N = int(input().split()[0])
+N, K = list(map(int, input().split()))
 a_list = list(map(int, input().split()))
-x_list = []
+counter = defaultdict(lambda: dict(left=0, right=0))
+total_right = 0
+total_left = 0
+for i, a in enumerate(a_list):
+    for j, b in enumerate(a_list):
+        if i == j:
+            continue
+        if i < j and a > b:
+            counter[i]["right"] += 1
+        if i > j and a > b:
+            counter[i]["left"] += 1
+    total_right += (counter[i]["right"] * K * (K + 1)) // 2
+    total_left += (counter[i]["left"] * (K - 1) * K) // 2
 
-for _ in range(N):
-    x = list(map(int, input().split()))
-    x_list.append(x)
-
-ans =
+mod = 10 ** 9 + 7
+ans = (total_left + total_right) % mod
 print(ans)
