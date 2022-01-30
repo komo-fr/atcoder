251bbb0d084cc1e11cc680a8ec9ac9efb9a60ee7
komo-fr <komo.mdrms@gmail.com>
2022-01-30 22:36:33 +0900

    [AC] C - Unexpressed

--- abc/193/c/main.py
+++ abc/193/c/main.py
@@ -2,11 +2,14 @@
 
 N = int(input().split()[0])
 ok_list = []
-for i in range(2, N // 2 + 1):
-    for j in range(2, N // 2 + 1):
+for i in range(2, int(N ** (1 / 2)) + 2):
+    for j in range(2, int(N ** (1 / 2)) + 2):
         if i ** j > N:
             break
+        # print(f"{i} ** {j} = {i ** j}")
         ok_list.append(i ** j)
-
+# print(len(ok_list))
 ans = N - len(set(ok_list))
+# print(len(set(ok_list)))
+
 print(ans)
