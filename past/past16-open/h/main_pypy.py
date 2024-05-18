4c89ab13cd66291c1aa7712ab67960a6bf5c7bae
komo-fr <komo.mdrms@gmail.com>
2024-05-18 18:34:35 +0900

    [AC] H - 休暇（PyPyで提出）

--- past/past16-open/h/main_pypy.py
+++ past/past16-open/h/main_pypy.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 N, M = list(map(int, input().split()))
 a_list = list(map(int, input().split()))
 
@@ -8,7 +7,8 @@ a_list = list(map(int, input().split()))
 # = i日目まで考慮したとき、（今日を含め）j日間宿題をした、（今日）宿題をしたかどうかがk（0:していない、1:している）
 # であるような状態の最大の楽しさとする
 # (N, N, 2)サイズ
-dp = np.full((N, M+2, 2), -float("inf"))
+# dp = np.full((N, N+1, 2), -float("inf"))
+dp = [[[-float("inf") for _ in range(2)] for _ in range(M+2)] for _ in range(N)]
 
 # 初日に宿題をしなかった場合
 dp[0][0][0] = a_list[0]  # 楽しさを得る
@@ -18,13 +18,14 @@ dp[0][1][1] = 0
 # 2日目からN日目まで考える
 for i in range(1, N):  # 実際の日数はi+1日目
     # i日目について考える
+    # for j in range(min(i+2, M)):  # 0〜i+1日（=最大N日）がありうる
     for j in range(min(i+2, M+2)):  # 0〜i+1日（=最大N日）がありうる
         # ここまででj日間宿題をしたケースについて考える
         # i日目に宿題をしない場合
-        dp[i][j-1][0] = max(dp[i-1][j-1][0], dp[i-1][j-1][1]) + a_list[i]
+        dp[i][j-1][0] = max([dp[i-1][j-1][0], dp[i-1][j-1][1]]) + a_list[i]
 
         # i日目に宿題をする場合、前日の宿題をしていないケースを持ってくる
         dp[i][j][1] = dp[i-1][j-1][0]
 
-ans = dp[N-1][M].max()
+ans = max(dp[N-1][M])
 print(int(ans))
